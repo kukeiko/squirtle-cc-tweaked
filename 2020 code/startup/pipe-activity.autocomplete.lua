f1a10abffe0bb779e3fcf5b604cffaa7c717a5e0
Magna Marek <magna.marek@gmail.com>
2022-03-20 17:51:28 +0100

    health: #2 delete "2020 code" folder kept "navigateTunnel()" though, as it is a good fn. will be committed later. rest we already have or no longer required. if we do, i pushed an archive branch

--- 2020 code/startup/pipe-activity.autocomplete.lua
+++
@@ -1,4 +0,0 @@
-local completion = require "cc.shell.completion"
-local complete = completion.build({completion.choice, {"autorun"}})
-shell.setCompletionFunction("apps/pipe-activity.lua", complete)
-shell.setAlias("pipe-activity", "apps/pipe-activity.lua")
