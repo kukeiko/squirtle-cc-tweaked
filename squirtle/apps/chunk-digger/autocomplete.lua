2c872302ec969159b1654780547791242c29b4b9
Magna Marek <magna.marek@gmail.com>
2022-03-20 20:00:53 +0100

    health: #2 move squirtle apps to new app folder

--- squirtle/apps/chunk-digger/autocomplete.lua
+++
@@ -1,4 +0,0 @@
-local completion = require "cc.shell.completion"
-local complete = completion.build({completion.choice, {"autorun"}})
-shell.setCompletionFunction("squirtle/apps/chunk-digger/app.lua", complete)
-shell.setAlias("chunk-digger", "squirtle/apps/chunk-digger/app.lua")
--- squirtle/apps/turtle/farm-lane/autocomplete.lua
+++
@@ -1,4 +0,0 @@
-local completion = require "cc.shell.completion"
-local complete = completion.build(nil, {completion.choice, {"turn-left", "turn-right"}})
-shell.setCompletionFunction("squirtle/apps/turtle/farm-lane/app.lua", complete)
-shell.setAlias("farm-lane", "squirtle/apps/turtle/farm-lane/app.lua")
