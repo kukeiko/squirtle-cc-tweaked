7ea2cfdd8d6c7c944c20c11e26981dcb44091895
Magna Marek <magna.marek@gmail.com>
2022-08-16 19:52:00 +0200

    feat: #14 use turtle_inventory instead of waiting for enter key when refueling

--- lib/squirtle/refuel/with-help-from-player.lua
+++ lib/squirtle/refuel/with-help-from-player.lua
@@ -1,4 +1,3 @@
-local Utils = require "utils"
 local Fuel = require "squirtle.fuel"
 local refuelFromInventory = require "squirtle.refuel.from-inventory"
 
@@ -9,8 +8,8 @@ return function(fuel)
     while Fuel.getFuelLevel() < fuel do
         local openFuel = fuel - Fuel.getFuelLevel()
         print(string.format("[help] not enough fuel, need %d more.", openFuel))
-        print("please put some into inventory, then hit enter.")
-        Utils.waitForUserToHitEnter()
+        print("please put some into inventory")
+        os.pullEvent("turtle_inventory")
         refuelFromInventory(openFuel)
     end
 end
