8725eab5dda73e5abdd4cd94d52d9f45f95a62c3
Magna Marek <magna.marek@gmail.com>
2024-06-01 10:03:53 +0200

    feat: add duck

---
+++ duck.lua
@@ -0,0 +1,24 @@
+local foo = require "squirtle.refuel"
+
+--    _
+-- __(.)<
+-- \___)
+local logo = {"    _  ", " __(.)<", " \\___) "}
+
+return function()
+    term.clear()
+    local screenWidth, screenHeight = term.getSize()
+    local logoWidth = logo[1]:len()
+    local logoHeight = #logo
+    local startY = math.floor((screenHeight - logoHeight) / 2)
+    local startX = math.floor((screenWidth - logoWidth) / 2)
+
+    for y = 1, #logo do
+        term.setCursorPos(startX, y + startY)
+        term.write(logo[y])
+    end
+
+    os.sleep(1)
+    term.setCursorPos(1, 1)
+    term.clear()
+end
