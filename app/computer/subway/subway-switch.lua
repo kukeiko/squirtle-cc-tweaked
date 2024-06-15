80a5651e14ecee8c5b65237ff25feb53e964725d
Magna Marek <magna.marek@gmail.com>
2024-06-15 05:39:29 +0200

    feat(subway-switch): make args mandatory

--- app/computer/subway/subway-switch.lua
+++ app/computer/subway/subway-switch.lua
@@ -10,12 +10,15 @@ end
 local function main(args)
     print("[subway-switch v2.0.0-dev] booting...")
 
-    SubwayService.signalDuration = tonumber(args[1]) or 2
-    SubwayService.maxDistance = tonumber(args[2]) or 3
+    local signalDuration = tonumber(args[1])
+    local maxDistance = tonumber(args[2])
 
-    if not SubwayService.host then
+    if not signalDuration or not maxDistance then
         return printUsage()
     end
+
+    SubwayService.signalDuration = signalDuration
+    SubwayService.maxDistance = maxDistance
 
     print("[signal-duration]", SubwayService.signalDuration)
     print("[max-distance]", SubwayService.maxDistance)
