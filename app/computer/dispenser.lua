59c4f1cb2509fb92eb87366f082179796497ecbf
Magna Marek <magna.marek@gmail.com>
2024-06-17 12:29:43 +0200

    release: io-network v6.3.0, dispenser v1.2.1

--- app/computer/dispenser.lua
+++ app/computer/dispenser.lua
@@ -35,7 +35,7 @@ function getListTitle()
 end
 
 EventLoop.run(function()
-    print("[dispenser v1.2.1-dev] connecting to storage service...")
+    print("[dispenser v1.2.1] connecting to storage service...")
 
     local storage = Rpc.nearest(StorageService)
 
