299ebb47c3f741ca3b38c7144f8c2023b117f781
Magna Marek <magna.marek@gmail.com>
2024-06-17 12:27:55 +0200

    feat(io-network): fill shulkers from silo-output

--- app/computer/io-network/process-shulkers.lua
+++ app/computer/io-network/process-shulkers.lua
@@ -3,6 +3,8 @@ local Inventory = require "inventory"
 return function()
     local success, e = pcall(function(...)
         local shulkers = Inventory.getInventories("shulker", true)
+        local siloOutputs = Inventory.getInventories("silo:output")
+        Inventory.distributeFromTag(siloOutputs, shulkers, "output", "input")
         local storages = Inventory.getInventories("storage")
         Inventory.distributeFromTag(storages, shulkers, "output", "input")
     end)
