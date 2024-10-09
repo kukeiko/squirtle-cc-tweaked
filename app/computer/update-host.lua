68a5b6cf9ec7bc814d132b3242d5149b364e98f5
Magna Marek <magna.marek@gmail.com>
2024-10-09 16:14:42 +0200

    feat(update-host): write startup file

--- app/computer/update-host.lua
+++ app/computer/update-host.lua
@@ -1,4 +1,5 @@
 package.path = package.path .. ";/?.lua"
+local Utils = require "lib.common.utils"
 local Rpc = require "lib.common.rpc"
 local EventLoop = require "lib.common.event-loop"
 local AppsService = require "lib.features.apps-service"
@@ -6,6 +7,8 @@ local DatabaseService = require "lib.common.database-service"
 
 local function main()
     print("[update-host v2.0.0-dev] booting...")
+
+    Utils.writeStartupFile("update-host")
 
     EventLoop.run(function()
         Rpc.server(AppsService)
