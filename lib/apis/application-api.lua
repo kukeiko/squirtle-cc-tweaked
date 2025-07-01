a2b91e3f235f8782381fe44b929093b4dcf6e5dc
Magna Marek <magna.marek@gmail.com>
2025-07-01 17:01:58 +0200

    refactor: #26 add application-api

---
+++ lib/apis/application-api.lua
@@ -0,0 +1,154 @@
+local Utils = require "lib.tools.utils"
+
+---@class ApplicationApi
+---@field versions table<string, string>
+local ApplicationApi = {folder = "apps", versions = {}}
+
+if Utils.isDev() then
+    ApplicationApi.folder = "dist"
+end
+
+function ApplicationApi.initAppVersions()
+    ---@type table<string, string>
+    local versions = {}
+    local subFolders = {"computer", "pocket", "turtle"}
+
+    for _, subFolder in pairs(subFolders) do
+        if fs.isDir(fs.combine(ApplicationApi.folder, subFolder)) then
+            for _, fileName in pairs(fs.list(fs.combine(ApplicationApi.folder, subFolder))) do
+                local appPath = fs.combine(ApplicationApi.folder, subFolder, fileName)
+                ---@type ApplicationMetadata
+                local metadata = dofile(appPath)
+                versions[appPath] = metadata.version
+                print(string.format("[%s] %s @ %s", subFolder, fileName, versions[appPath]))
+            end
+        end
+    end
+
+    ApplicationApi.versions = versions
+end
+
+---@param path string
+---@param withContent? boolean
+---@return Application
+function ApplicationApi.getApplication(path, withContent)
+    local app = {name = fs.getName(path), version = ApplicationApi.versions[path] or "?", path = path}
+
+    if withContent then
+        local file = fs.open(path, "r")
+        app.content = file.readAll()
+        file.close()
+    end
+
+    return app
+end
+
+---@param folder string
+---@param withContent? boolean
+---@param filter? string[]
+---@return Application[]
+local function getApps(folder, withContent, filter)
+    if not fs.isDir(folder) then
+        return {}
+    end
+
+    withContent = withContent or false
+
+    ---@type string[]
+    local fileNames = fs.list(folder)
+
+    if filter and #filter > 0 then
+        fileNames = Utils.filter(fileNames, function(fileName)
+            return Utils.indexOf(filter, fileName) ~= nil
+        end)
+    end
+
+    return Utils.map(fileNames, function(fileName)
+        local path = fs.combine(folder, fileName)
+        ---@type Application
+        local app = {name = fileName, version = ApplicationApi.versions[path], path = path}
+
+        if withContent then
+            local file = fs.open(path, "r")
+            app.content = file.readAll()
+            file.close()
+        end
+
+        return app
+    end)
+end
+
+---@param folder string
+---@param apps Application[]
+local function setApps(folder, apps)
+    if Utils.isDev() and os.getComputerLabel() ~= "Database" then
+        return
+    end
+
+    for _, app in pairs(apps) do
+        local path = fs.combine(folder, app.name)
+        local file = fs.open(path, "w")
+        file.write(app.content or "")
+        file.close()
+        ApplicationApi.versions[path] = app.version
+    end
+end
+
+---@param withContent? boolean
+---@param filter? string[]
+---@return Application[]
+function ApplicationApi.getPocketApps(withContent, filter)
+    return getApps(fs.combine(ApplicationApi.folder, "pocket"), withContent, filter)
+end
+
+---@param apps Application[]
+---@param inRoot? boolean
+function ApplicationApi.setPocketApps(apps, inRoot)
+    local path = "/"
+
+    if not inRoot then
+        path = fs.combine(ApplicationApi.folder, "pocket")
+    end
+
+    setApps(path, apps)
+end
+
+---@param withContent? boolean
+---@param filter? string[]
+---@return Application[]
+function ApplicationApi.getTurtleApps(withContent, filter)
+    return getApps(fs.combine(ApplicationApi.folder, "turtle"), withContent, filter)
+end
+
+---@param apps Application[]
+---@param inRoot? boolean
+function ApplicationApi.setTurleApps(apps, inRoot)
+    local path = "/"
+
+    if not inRoot then
+        path = fs.combine(ApplicationApi.folder, "turtle")
+    end
+
+    setApps(path, apps)
+end
+
+---@param withContent? boolean
+---@param filter? string[]
+---@return Application[]
+function ApplicationApi.getComputerApps(withContent, filter)
+    return getApps(fs.combine(ApplicationApi.folder, "computer"), withContent, filter)
+end
+
+---@param apps Application[]
+---@param inRoot? boolean
+function ApplicationApi.setComputerApps(apps, inRoot)
+    local path = "/"
+
+    if not inRoot then
+        path = fs.combine(ApplicationApi.folder, "computer")
+    end
+
+    setApps(path, apps)
+end
+
+return ApplicationApi
