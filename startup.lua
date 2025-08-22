75704cab9a77d63c8a4798346c2f5eb26f778625
Magna Marek <magna.marek@gmail.com>
2025-08-22 04:23:09 +0200

    feat: [storage] #35 adapt startup for power items chest

--- startup.lua
+++ startup.lua
@@ -1,7 +1,7 @@
 local label = os.getComputerLabel()
 local programs = {
     ["Database"] = "app/computer/database.lua",
-    ["Storage"] = "app/computer/storage.lua",
+    ["Storage"] = "app/computer/storage.lua bottom",
     ["Storage Workers"] = "app/computer/storage-workers.lua",
     ["Home"] = "app/computer/dispenser.lua",
     ["Crafty"] = "app/turtle/io-crafter.lua",
