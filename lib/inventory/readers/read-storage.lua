4518c72260cbe59bbc52e9152783f68aaf5ee047
Magna Marek <magna.marek@gmail.com>
2025-08-24 10:28:54 +0200

    feat: [storage] #35 support dispensing non-stackables

--- lib/inventory/readers/read-storage.lua
+++ lib/inventory/readers/read-storage.lua
@@ -2,26 +2,6 @@ local Utils = require "lib.tools.utils"
 local InventoryPeripheral = require "lib.inventory.inventory-peripheral"
 local Inventory = require "lib.inventory.inventory"
 
----@param stacks ItemStacks
----@return boolean
-local function isMonoTypeStacks(stacks)
-    if Utils.isEmpty(stacks) then
-        return false
-    end
-
-    local name
-
-    for _, stack in pairs(stacks) do
-        if not name then
-            name = stack.name
-        elseif name and stack.name ~= name then
-            return false
-        end
-    end
-
-    return true
-end
-
 ---@param name string
 ---@param stacks table<integer, ItemStack>
 ---@return Inventory
@@ -33,32 +13,27 @@ return function(name, stacks)
     ---@type table<string, integer>
     local items = {}
 
-    if isMonoTypeStacks(stacks) then
-        local first = Utils.first(stacks) --[[@as ItemStack]]
-        items[first.name] = 0
-        ---@type ItemStack
-        local template = {name = first.name, count = 0, displayName = first.displayName, maxCount = first.maxCount}
+    ---@type ItemStack?
+    local templateStack
 
-        for index = 1, InventoryPeripheral.getSize(name) do
-            ---@type InventorySlot
-            local slot = {index = index, tags = slotTags, permanent = true}
-            slots[index] = slot
-            local stack = stacks[index]
+    for index = 1, InventoryPeripheral.getSize(name) do
+        ---@type InventorySlot
+        local slot = {index = index, tags = slotTags, permanent = true}
+        slots[index] = slot
+        local stack = stacks[index]
 
-            if stack then
-                stack.maxCount = stack.maxCount - 1
+        if stack then
+            if not templateStack or stack.name ~= templateStack.name then
+                templateStack = Utils.copy(stack)
+                templateStack.count = 0
                 stack.count = stack.count - 1
-                items[stack.name] = items[stack.name] + stack.count
-            else
-                stacks[index] = Utils.copy(template)
+                stack.maxCount = stack.maxCount - 1
             end
-        end
-    else
-        for index, stack in pairs(stacks) do
-            slots[index] = {index = index, tags = slotTags, permanent = true}
-            stack.maxCount = stack.maxCount - 1
-            stack.count = stack.count - 1
+
+            stacks[index] = stack
             items[stack.name] = (items[stack.name] or 0) + stack.count
+        elseif templateStack then
+            stacks[index] = Utils.copy(templateStack)
         end
     end
 
