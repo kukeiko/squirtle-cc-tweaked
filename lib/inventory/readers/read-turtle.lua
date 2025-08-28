4c2d483a21f208838fc2c5e455c661703d3335b4
Magna Marek <magna.marek@gmail.com>
2025-08-28 08:02:31 +0200

    feat: [storage] change turtle inventory to only use last 4 slots for i/o - to prevent turtle losing access to an empty slot - aligns nicely with max. carried shulkers of 8, leaving 4 slots for disk drive + item shuffling

--- lib/inventory/readers/read-turtle.lua
+++ lib/inventory/readers/read-turtle.lua
@@ -1,8 +1,27 @@
-local readCommon = require "lib.inventory.readers.read-common"
+local Inventory = require "lib.inventory.inventory"
+local ItemStock = require "lib.inventory.item-stock"
 
 ---@param name string
 ---@param stacks table<integer, ItemStack>
 ---@return Inventory
 return function(name, stacks)
-    return readCommon(name, "turtle", stacks, nil, {input = true, output = true}, nil, true)
+    ---@type table<integer, InventorySlot>
+    local slots = {}
+    ---@type InventorySlotTags
+    local ioSlotTags = {input = true, output = true}
+    ---@type ItemStack[]
+    local ioStacks = {}
+
+    for slot = 1, 16 do
+        if slot < 13 then
+            slots[slot] = {index = slot, tags = {}}
+        else
+            slots[slot] = {index = slot, tags = ioSlotTags}
+            ioStacks[slot] = stacks[slot]
+        end
+    end
+
+    local items = ItemStock.fromStacks(ioStacks)
+
+    return Inventory.create(name, "turtle", ioStacks, slots, true, nil, items)
 end
